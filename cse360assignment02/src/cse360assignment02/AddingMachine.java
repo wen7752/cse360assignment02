e1941edd9c2b78db5bfa0aa51091822ca12dc661
wen7752 <wen7752@users.noreply.github.com>
2020-10-05 06:17:20 -0700

    Second version

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,32 +1,77 @@
 /**  
-* AddingMachine.java - First version
+* AddingMachine.java - Second version
 * @author Wen Fang
-* @version 1.0
+* @data 10/05/2020
+* @version 2.0
 */ 
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
 
+	//initialize the variables 
+	private int total;
+	private String result = "";
+		
+	  	  public AddingMachine () {
+			 total = 0;
+			 result = "0";
+		  }
+		  
+	  	  //return the total 
+		  public int getTotal () {
+		    return total;
+		  }
+		  
+		/**
+		 * adding the value to total and store the value
+		 * into the result string.
+		 */
+		  public void add (int value) {
+			  
+			  total = total + value; //add to the result
+			  result = result + " + " + value; //add to the string
+		  }
+		  /**
+		   * subtracting the value to total and store the value
+		   * into the result string.
+		   */
+		  public void subtract (int value) {
+			  
+			  total = total - value;
+			  result = result + " - " + value;
+		  }
+		  
+		  
+		  /**
+		   * return the stored string
+		   */
+		  public String toString () {
+		    return result.toString();
+		  }
+		  
+		  /**
+		   *Clear the memory 
+		   */
+		  public void clear() {
+			  
+			  total = 0;
+			  result = "0";
+		  }
+	  
   
-  public void subtract (int value) {
-  }
-
-  public String toString () {
-    return "";
-  }
-
-  public void clear() {
-  }
+  
+  public static void main(String[] args) {
+		
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		
+		//Print out the toString method 
+		System.out.println(myCalculator.toString());
+		//System.out.println(myCalculator.getTotal());
+		
+		
+	}
 }
