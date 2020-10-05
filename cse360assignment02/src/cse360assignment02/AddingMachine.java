3bee7026050551d73884bc4c5fb933da8fe06c74
wen7752 <wen7752@users.noreply.github.com>
2020-10-05 04:49:47 -0700

    First commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+/**  
+* AddingMachine.java - First version
+* @author Wen Fang
+* @version 1.0
+*/ 
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
